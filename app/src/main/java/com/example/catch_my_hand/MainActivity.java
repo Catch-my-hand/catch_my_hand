cd50d6e9c56da596b2c401c14e3baaf99fc24169
Dong-Seung-hyeon <94991070+Dong-Seung-hyeon@users.noreply.github.com>
2022-02-10 03:10:10 +0900

    label = 내 손을 잡아로 설정 java 파일과 layout 파일추가

--- app/src/main/java/com/example/catch_my_hand/MainActivity.java
+++ app/src/main/java/com/example/catch_my_hand/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.catch_my_hand;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
