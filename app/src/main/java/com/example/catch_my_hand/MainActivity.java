b61c0ad7c509b414ae2ce2d47a48774f1f2f3b67
Dong-Seung-hyeon <94991070+Dong-Seung-hyeon@users.noreply.github.com>
2022-02-11 15:13:49 +0900

    label = 내 손을 잡아로 설정 java 파일과 layout 파일추가

--- app/src/main/java/com/example/catch_my_hand/MainActivity.java
+++ app/src/main/java/com/example/catch_my_hand/MainActivity.java
@@ -9,10 +9,19 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView1;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.textView1);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        textView1=(TextView) findViewById(R.id.textView1);
+        textView2=(TextView) findViewById(R.id.textView2);
     }
 }
