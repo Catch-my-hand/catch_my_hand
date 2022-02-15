1e25b30bfe6825c3708255e7df490e5f06e3d42e
Dong-Seung-hyeon <kj94gakt@naver.com>
2022-02-15 23:52:16 +0900

    로그인, 비밀번호, 로그인버튼, 회원가입버튼, 아이디/비밀번호 찾기 제작 버튼과 버튼사이 간격 띄움 텍스트 아이디와 버튼 아이디 추가 로그인, 회원가입, 아이디/비밀번호 찾기 버튼 연결

--- app/src/main/java/com/example/catch_my_hand/Animal_info_Activity.java
+++ app/src/main/java/com/example/catch_my_hand/Animal_info_Activity.java
@@ -1,4 +1,15 @@
 package com.example.catch_my_hand;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Animal_info_Activity {
+
+    @Override
+    proteccted void onCreate(@Nullable Bundle saveInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.animal_info);
+    }
 }
--- app/src/main/java/com/example/catch_my_hand/LoginActivity.java
+++ app/src/main/java/com/example/catch_my_hand/LoginActivity.java
@@ -1,12 +1,12 @@
 package com.example.catch_my_hand;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     Button btn1;
@@ -22,23 +22,23 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        TextView textView = (TextView)findViewById(R.id.textView1);
+        TextView textView = (TextView) findViewById(R.id.textView1);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        textView1=(TextView) findViewById(R.id.textView1);
-        textView2=(TextView) findViewById(R.id.textView2);
-        textView3=(TextView) findViewById(R.id.textView3);
-        textView4=(TextView) findViewById(R.id.textView4);
+        textView1 = (TextView) findViewById(R.id.textView1);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+        textView4 = (TextView) findViewById(R.id.textView4);
 
-        btn1=(Button) findViewById(R.id.btn1);
-        btn2=(Button) findViewById(R.id.btn2);
-        btn3=(Button) findViewById(R.id.btn3);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
 
         Button btn1 = (Button) findViewById(R.id.btn1);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
@@ -47,7 +47,7 @@ public class LoginActivity extends AppCompatActivity {
         Button btn2 = (Button) findViewById(R.id.btn2);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Join_check_Activity.class);
                 startActivity(intent);
             }
@@ -56,7 +56,7 @@ public class LoginActivity extends AppCompatActivity {
         Button btn3 = (Button) findViewById(R.id.btn3);
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Id_search_Activity.class);
                 startActivity(intent);
             }
