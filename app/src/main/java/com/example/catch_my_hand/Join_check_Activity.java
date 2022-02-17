bf6b65e3b6c256bf96465a4bbe410f1fd0757680
Dong-Seung-hyeon <kj94gakt@naver.com>
2022-02-18 00:33:12 +0900

    로그인, 비밀번호, 로그인버튼, 회원가입버튼, 아이디/비밀번호 찾기 제작 버튼과 버튼사이 간격 띄움 텍스트 아이디와 버튼 아이디 추가 로그인, 회원가입, 아이디/비밀번호 찾기 버튼 연결

--- app/src/main/java/com/example/catch_my_hand/Join_check_Activity.java
+++ app/src/main/java/com/example/catch_my_hand/Join_check_Activity.java
@@ -1,15 +1,46 @@
 package com.example.catch_my_hand;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Join_check_Activity extends AppCompatActivity{
 
+    Button btn1;
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+
     @Override
     protected void onCreate(@Nullable Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.join_check);
+
+        TextView textView = (TextView)findViewById(R.id.textView1);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        textView1=(TextView) findViewById(R.id.textView1);
+        textView2=(TextView) findViewById(R.id.textView2);
+        textView3=(TextView) findViewById(R.id.textView3);
+        textView4=(TextView) findViewById(R.id.textView4);
+
+        btn1=(Button) findViewById(R.id.btn1);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
