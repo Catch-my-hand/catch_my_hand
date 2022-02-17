3d9cb246b1ba2b33826e7d7e7ecab92e3fc251aa
chaerimi <vmflszbdj00@naver.com>
2022-02-18 00:34:48 +0900

    아이디 / 비밀번호 찾기 페이지 제작

--- app/src/main/java/com/example/catch_my_hand/JoinActivity.java
+++ app/src/main/java/com/example/catch_my_hand/JoinActivity.java
@@ -1,15 +1,31 @@
 package com.example.catch_my_hand;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class JoinActivity extends AppCompatActivity{
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class JoinActivity extends AppCompatActivity {
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
+    TextView textView6;
+    TextView textView7;
 
     @Override
-    protected void onCreate(@Nullable Bundle saveInstanceState) {
-        super.onCreate(saveInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.join);
+
+
+        }
     }
-}
+
