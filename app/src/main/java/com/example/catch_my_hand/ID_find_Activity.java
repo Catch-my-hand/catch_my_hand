01895ea935c24f8e354bba761d14382bf98d4292
Dong-Seung-hyeon <kj94gakt@naver.com>
2022-02-20 16:17:47 +0900

    로그인, 비밀번호, 로그인버튼, 회원가입버튼, 아이디/비밀번호 찾기 제작 버튼과 버튼사이 간격 띄움 텍스트 아이디와 버튼 아이디 추가 로그인, 회원가입, 아이디/비밀번호 찾기 버튼 연결

---
+++ app/src/main/java/com/example/catch_my_hand/ID_find_Activity.java
@@ -0,0 +1,15 @@
+package com.example.catch_my_hand;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ID_find_Activity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.id_find);
+    }
+}
---
+++ app/src/main/java/com/example/catch_my_hand/PW_find_Activity.java
@@ -0,0 +1,15 @@
+package com.example.catch_my_hand;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PW_find_Activity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.pw_find);
+    }
+}
